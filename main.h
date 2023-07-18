80e7f0962e8d98bbd8febfa9eb2d61a8cae74df1
Juan Ferraro <135638564+DominusJP@users.noreply.github.com>
2023-07-18 12:56:04 -0300

    Update main.h

--- main.h
+++ main.h
@@ -1,13 +1,25 @@
 #ifndef MAIN_H
 #define MAIN_H
 
-/* Librerias (some just in case) */
-#include <stdio.h>
-#include <stdarg.h>
-#include <stdlib.h>
-#include <string.h>
+/**
+ * struct toprint - Type of argument and function associated info
+ * @letter : type of argument (format[i + 1])
+ * @f : function associated
+ */
 
-/* Functions */
-int _putchar(char c);
+typedef struct toprint
+	{
+		char *letter;
+		int (*f)(va_list vargs);
+	} toprint_t;
+
+/* functions */
+int _printf(const char *format, ...);
+int _putchar(char);
+int print_c(va_list vargs);
+int print_s(va_list vargs);
+int print_prc(va_list vargs);
+int print_di(va_list vargs);
+int elegir_funcion(va_list vargs, char letter);
 
 #endif /* MAIN_H */
